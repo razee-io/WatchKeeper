458f289e0a79e087ae7e583566e56dac60631a89
Alex Lewitt <48691328+alewitt2@users.noreply.github.com>
2020-06-24 13:54:38 -0400

    Update: move envs to volumes to allow for common id config (#140)
    * Update: move envs to volumes to allow for common id config
    
    Moving to volumes so that we can start allowing use of a common razee-identity configmap/secret that will be shared with clusterSubsription and possibly others in the future.
    
    * Fix: indent volume mounts correctly

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 /**
-* Copyright 2019 IBM Corp. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2019 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const touch = require('touch');
 const promiseRetry = require('promise-retry');
+const fs = require('fs-extra');
 const log = require('./bunyan-api').createLogger('Index');
 
 const Util = require('./controllers/Util');
@@ -65,6 +66,7 @@ async function main() {
 
 async function init() {
   try {
+    await setEnvs();
     util = await Util.fetch();
   } catch (e) {
     const Messenger = require('./razeedash/Messenger');
@@ -91,3 +93,58 @@ promiseRetry({ retries: 5 },
   retry => {
     return init().catch(retry);
   }).catch(err => log.error(`Failed to init watch-keeper | ${err}`));
+
+async function setEnvs() {
+  const env = process.env;
+
+  // Razee Config
+  if (await fs.pathExists('envs/watch-keeper-config/RAZEEDASH_URL')) {
+    env.RAZEEDASH_URL = (await fs.readFile('envs/watch-keeper-config/RAZEEDASH_URL', 'utf8')).trim();
+  } else if (await fs.pathExists('envs/razee-identity-config/RAZEE_API')) {
+    let razeeApi = (await fs.readFile('envs/razee-identity-config/RAZEE_API', 'utf8')).trim();
+    env.RAZEEDASH_URL = `${razeeApi.replace(/\/+$/, '')}/api/v2`;
+  } else {
+    log.error('failed to find Razee url to post data to. exiting(1)');
+    process.exit(1);
+  }
+  if (await fs.pathExists('envs/razee-identity-secret/RAZEE_ORG_KEY')) {
+    env.RAZEEDASH_ORG_KEY = (await fs.readFile('envs/razee-identity-secret/RAZEE_ORG_KEY', 'utf8')).trim();
+  } else if (await fs.pathExists('envs/watch-keeper-secret/RAZEEDASH_ORG_KEY')) {
+    env.RAZEEDASH_ORG_KEY = (await fs.readFile('envs/watch-keeper-secret/RAZEEDASH_ORG_KEY', 'utf8')).trim();
+  }
+  if (await fs.pathExists('envs/razee-identity-config/CLUSTER_ID')) {
+    env.CLUSTER_ID_OVERRIDE = (await fs.readFile('envs/razee-identity-config/CLUSTER_ID', 'utf8')).trim();
+  } else if (await fs.pathExists('envs/watch-keeper-config/CLUSTER_ID_OVERRIDE')) {
+    env.CLUSTER_ID_OVERRIDE = (await fs.readFile('envs/watch-keeper-config/CLUSTER_ID_OVERRIDE', 'utf8')).trim();
+  }
+  if (await fs.pathExists('envs/razee-identity-config/CLUSTER_NAME')) {
+    env.DEFAULT_CLUSTER_NAME = (await fs.readFile('envs/razee-identity-config/CLUSTER_NAME', 'utf8')).trim();
+  } else if (await fs.pathExists('envs/watch-keeper-config/DEFAULT_CLUSTER_NAME')) {
+    env.DEFAULT_CLUSTER_NAME = (await fs.readFile('envs/watch-keeper-config/DEFAULT_CLUSTER_NAME', 'utf8')).trim();
+  }
+
+  // Watch-keeper Specific Config
+  if (await fs.pathExists('envs/watch-keeper-config/START_DELAY_MAX')) {
+    env.START_DELAY_MAX = (await fs.readFile('envs/watch-keeper-config/START_DELAY_MAX', 'utf8')).trim();
+  }
+  if (await fs.pathExists('envs/watch-keeper-config/CONFIG_NAMESPACE')) {
+    env.CONFIG_NAMESPACE = (await fs.readFile('envs/watch-keeper-config/CONFIG_NAMESPACE', 'utf8')).trim();
+  }
+  if (await fs.pathExists('envs/watch-keeper-config/KUBECONFIG')) {
+    env.KUBECONFIG = (await fs.readFile('envs/watch-keeper-config/KUBECONFIG', 'utf8')).trim();
+  } else {
+    env.KUBECONFIG = '/etc/kubernetes/admin-kubeconfig';
+  }
+  if (await fs.pathExists('envs/watch-keeper-config/VALIDATE_INTERVAL')) {
+    env.VALIDATE_INTERVAL = (await fs.readFile('envs/watch-keeper-config/VALIDATE_INTERVAL', 'utf8')).trim();
+  }
+  if (await fs.pathExists('envs/watch-keeper-config/POLL_INTERVAL')) {
+    env.POLL_INTERVAL = (await fs.readFile('envs/watch-keeper-config/POLL_INTERVAL', 'utf8')).trim();
+  }
+  if (await fs.pathExists('envs/watch-keeper-config/CLEAN_START_INTERVAL')) {
+    env.CLEAN_START_INTERVAL = (await fs.readFile('envs/watch-keeper-config/CLEAN_START_INTERVAL', 'utf8')).trim();
+  }
+  if (await fs.pathExists('envs/watch-keeper-config/LOG_LEVEL')) {
+    env.LOG_LEVEL = (await fs.readFile('envs/watch-keeper-config/LOG_LEVEL', 'utf8')).trim();
+  }
+}
